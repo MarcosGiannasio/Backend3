ad0fdf941662f9ceda05e13e8b29bc6a423fe372
Marcos Giannasio <marcosgiannasio@gmai.com>
2024-12-30 23:59:12 -0300

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+//const cluster          = require("node:cluster")
+//const { cpus }         = require("node:os")
+const { startServer }  = require("./src/server.js");
+//const { logger }       = require("./src/utils/logger.js");
+//const numeroDeCpu      = cpus().length
+
+
+startServer()
---
+++ readme.md
@@ -0,0 +1,2 @@
+Proyecto Backend
+Marcos Giannasio
